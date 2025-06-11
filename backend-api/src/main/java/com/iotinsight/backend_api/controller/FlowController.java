f13ff2d397572f8e727868a29bbd87222eb1721f
achavellir <rishiachavelli@webster.edu>
2025-06-11 14:45:03 -0400

    fix: map FlowController to /flows plural

--- backend-api/src/main/java/com/iotinsight/backend_api/controller/FlowController.java
+++ backend-api/src/main/java/com/iotinsight/backend_api/controller/FlowController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/flow")
+@RequestMapping("/flows")
 public class FlowController {
     static final List<Alert> alerts = new ArrayList<>();
 
